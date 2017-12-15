d9ac6b321bcc7de3af3333ab14285df3f77a3982
BajanVlogs <kjolly544@gmail.com>
2017-12-14 23:09:35 -0500

    STUPID MISTAKE MY BAD

--- src/FactionUI/Main.php
+++ src/FactionUI/Main.php
@@ -1,4 +1,27 @@
-public function onCommand(CommandSender $sender, Command $cmd, string $label,array $args) : bool {
+<?php
+
+namespace FactionUI;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\ConsoleCommandSender;
+
+class Main extends PluginBase implements Listener {
+	
+    public function onEnable() {
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+		if($api === null){
+			$this->getServer()->getPluginManager()->disablePlugin($this);			
+		}
+    }
+	
+    public function onCommand(CommandSender $sender, Command $cmd, string $label,array $args) : bool {
 		switch($cmd->getName()){
 			case "factionui":
 				if($sender instanceof Player) {
